f3ed524e2bfcbc02fd848f6b6a4223cc619cd07c
henry-49 <henryswebdevelopment@gmail.com>
2025-09-23 19:46:51 +0200

    Add topBannerOverlay class to TopBanner conponent

--- src/components/topbanner/TopBanner.jsx
+++ src/components/topbanner/TopBanner.jsx
@@ -1,16 +1,25 @@
 import React, { Component, Fragment } from 'react'
-import {Container, Row, Col} from "react-bootstrap";
+import {Container, Row, Col, Button} from "react-bootstrap";
 import '../../asset/css/custom.css';
+import "../../asset/css/bootstrap.min.css";
 
 
 class TopBanner extends Component {
   render() {
     return (
       <Fragment>
-        <Container className="topFixedBanner" fluid>
-          <Row>
-            <Col>1 of 1</Col>
-          </Row>
+        <Container className="topFixedBanner p-0" fluid>
+          <div className="topBannerOverlay">
+            <Container className="topContent">
+              <Row>
+                <Col className="text-center">
+                  <h1>Tech Guru Consult</h1>
+                  <h4>Your one-stop solution for tech consulting</h4>
+                  <Button variant="primary">Let's Get Started</Button>
+                </Col>
+              </Row>
+            </Container>
+          </div>
         </Container>
       </Fragment>
     );
