7faa207b5ef41e8bc3749a0dcefff69e9cdc713a
henry-49 <henryswebdevelopment@gmail.com>
2025-09-23 22:09:45 +0200

    Add Service Component and custom css style

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import TopBanner from './components/topbanner/TopBanner';
+import Services from './components/services/Services';
 
 
 function App() {
   return (
     <div>
-      <TopBanner  />    
+      <TopBanner  />
+      <Services />
     </div>
   );
 }
