a4abb299a84e12d31731f64351501462ab8fc860
henry-49 <henryswebdevelopment@gmail.com>
2025-09-30 00:51:18 +0200

    Add countUp to animate number reviews On Scroll

--- src/components/summary/Summary.jsx
+++ src/components/summary/Summary.jsx
@@ -5,7 +5,7 @@ import { faCheckSquare } from '@fortawesome/free-solid-svg-icons';
 import { faGlobe } from '@fortawesome/free-solid-svg-icons';
 import { faLaptop} from '@fortawesome/free-solid-svg-icons';
 import { faStar } from '@fortawesome/free-solid-svg-icons';
-
+import CountUp from "react-countup";
 
 class Summary extends Component {
   render() {
@@ -19,21 +19,39 @@ class Summary extends Component {
                   <Row style={{ marginTop: "20%" }}>
                     <Col>
                       <FontAwesomeIcon icon={faGlobe} className="iconProject" />
-                      <h1 className="countNumber">35+</h1>
+                      <h1 className="countNumber">
+                        <CountUp start={0} end={35} delay={0} enableScrollSpy>
+                          {({ countUpRef }) => <span ref={countUpRef} />}
+                        </CountUp>
+                        +
+                      </h1>
                       <h4 className="countTitle">Projects Completed</h4>
                       <hr className="hrTitle" />
                     </Col>
 
                     <Col>
-                    <FontAwesomeIcon icon={faLaptop} className="iconProject" />
-                      <h1 className="countNumber">22+</h1>
+                      <FontAwesomeIcon
+                        icon={faLaptop}
+                        className="iconProject"
+                      />
+                      <h1 className="countNumber">
+                        <CountUp start={0} end={22} delay={0} enableScrollSpy>
+                          {({ countUpRef }) => <span ref={countUpRef} />}
+                        </CountUp>
+                        +
+                      </h1>
                       <h4 className="countTitle">Happy Clients</h4>
                       <hr className="hrTitle" />
                     </Col>
 
                     <Col>
-                    <FontAwesomeIcon icon={faStar} className="iconProject" />
-                      <h1 className="countNumber">15+</h1>
+                      <FontAwesomeIcon icon={faStar} className="iconProject" />
+                      <h1 className="countNumber">
+                        <CountUp start={0} end={15} delay={0} enableScrollSpy>
+                          {({ countUpRef }) => <span ref={countUpRef} />}
+                        </CountUp>
+                        +
+                      </h1>
                       <h4 className="countTitle">Client Reviews</h4>
                       <hr className="hrTitle" />
                     </Col>
