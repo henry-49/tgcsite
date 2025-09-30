092a4d67a92f1b4e6a619f7c3c6fa9dcd2a2cedb
henry-49 <henryswebdevelopment@gmail.com>
2025-10-01 00:10:38 +0200

    Add image and custom css for course component

--- src/components/courses/Courses.jsx
+++ src/components/courses/Courses.jsx
@@ -11,15 +11,81 @@ import { Col, Container, Row } from 'react-bootstrap'
           <Row>
             <Col lg={6} md={12} sm={12}>
               <Row>
-                <Col lg={6} md={6} sm={12}></Col>
-                <Col lg={6} md={6} sm={12}></Col>
+                <Col lg={6} md={6} sm={12} className="p-2">
+                  <img
+                    className="courseImg"
+                    src="https://image.freepik.com/free-photo/learner-lession_1098-14155.jpg"
+                  />
+                </Col>
+                <Col lg={6} md={6} sm={12}>
+                  <h5 className="float-start serviceName">Laravel 10</h5>
+                  <p className="float-start serviceDescription">
+                    Laravel 10 - Build Advance Ecommerce Project A-Z Some quick
+                    example text to build on the card title and make up the bulk
+                    of the card's content.
+                  </p>
+                  <a className="courseViewMore float-start" href="#">
+                    View Details
+                  </a>
+                </Col>
+
+                <Col lg={6} md={6} sm={12} className="p-2">
+                  <img
+                    className="courseImg"
+                    src="https://image.freepik.com/free-photo/coach-by-whiteboard_1098-12976.jpg"
+                  />
+                </Col>
+                <Col lg={6} md={6} sm={12}>
+                  <h5 className="float-start serviceName">Laravel 10</h5>
+                  <p className="float-start serviceDescription">
+                    Laravel 10 - Build Advance Ecommerce Project A-Z Some quick
+                    example text to build on the card title and make up the bulk
+                    of the card's content.
+                  </p>
+                  <a className="courseViewMore float-start" href="#">
+                    View Details
+                  </a>
+                </Col>
               </Row>
             </Col>
 
             <Col lg={6} md={6} sm={12}>
               <Row>
-                <Col lg={6} md={6} sm={12}></Col>
-                <Col lg={6} md={6} sm={12}></Col>
+                <Col lg={6} md={6} sm={12} className="p-2">
+                  <img
+                    className="courseImg"
+                    src="https://image.freepik.com/free-photo/coach-by-whiteboard_1098-12970.jpg"
+                  />
+                </Col>
+                <Col lg={6} md={6} sm={12}>
+                  <h5 className="float-start serviceName">Laravel 10</h5>
+                  <p className="float-start serviceDescription">
+                    Laravel 10 - Build Advance Ecommerce Project A-Z Some quick
+                    example text to build on the card title and make up the bulk
+                    of the card's content.
+                  </p>
+                  <a className="courseViewMore float-start" href="#">
+                    View Details
+                  </a>
+                </Col>
+
+                <Col lg={6} md={6} sm={12} className="p-2">
+                  <img
+                    className="courseImg"
+                    src="https://image.freepik.com/free-photo/training-managers_1098-16067.jpg"
+                  />
+                </Col>
+                <Col lg={6} md={6} sm={12}>
+                  <h5 className="float-start serviceName">Laravel 10</h5>
+                  <p className="float-start serviceDescription">
+                    Laravel 10 - Build Advance Ecommerce Project A-Z Some quick
+                    example text to build on the card title and make up the bulk
+                    of the card's content.
+                  </p>
+                  <a className="courseViewMore float-start" href="#">
+                    View Details
+                  </a>
+                </Col>
               </Row>
             </Col>
           </Row>
