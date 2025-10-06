a10120f4f1e3e037a5f9f2a53bd8b820cade1535
henry-49 <henryswebdevelopment@gmail.com>
2025-10-06 18:47:36 +0200

    installed React-Type and add custom css for about details

--- src/components/about/AboutUs.jsx
+++ src/components/about/AboutUs.jsx
@@ -1,10 +1,11 @@
-import React, { Component, Fragment } from "react"
+import React, { Component, Fragment} from "react"
 import { Col, Container, Row } from "react-bootstrap"
 import face from "../../asset/images/face.png";
+import { ReactTyped } from "react-typed";
 
 class AboutUs extends Component {
-  render() {
 
+  render() {
     return (
       <Fragment>
         <Container className="text-center">
@@ -12,11 +13,35 @@ class AboutUs extends Component {
           <div className="buttom"></div>
           <Row>
             <Col lg={6} md={6} sm={12}>
-                <div className="aboutUsImage">
-                    <img className="aboutImg" src={face} alt="" />
-                </div>
+              <div className="aboutUsImage">
+                <img className="aboutImg" src={face} alt="" />
+              </div>
             </Col>
-            <Col lg={6} md={6} sm={12}></Col>
+            <Col lg={6} md={6} sm={12}>
+              <div className="aboutUsBody">
+                <h2 className="aboutUsGreeting">Hi There, we are</h2>
+                <h2 className="aboutUsTitle">Tech Solutions Consult</h2>
+                <h3 className="aboutUsDescription">
+                  We Build responsive and high-performing <br></br>
+                  {" "}
+                  <ReactTyped
+                    strings={[
+                      "Websites 🌐",
+                      "Mobile Apps 📱",
+                      "Software Solutions 💻",
+                      "Software Test Automation 🧪",
+                      "Design with Animations 🎨✨",
+                    ]}
+                    typeSpeed={80}
+                    backSpeed={50}
+                    backDelay={1500}
+                    loop
+                    showCursor
+                    cursorChar="|"
+                  />
+                </h3>
+              </div>
+            </Col>
           </Row>
         </Container>
       </Fragment>
