6cc2296eb67e89fa0a831e82d38950cbf264f491
henry-49 <henryswebdevelopment@gmail.com>
2025-10-10 13:01:39 +0200

    Add AppRouter and installed react-router-dom

---
+++ src/router/AppRouter.jsx
@@ -0,0 +1,28 @@
+import React, { Component, Fragment } from "react";
+import { Row, Card, Col, Container, Button, Form } from "react-bootstrap";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import HomePage from "../pages/HomePage";
+import AboutPage from "../pages/AboutPage";
+import ServicePage from "../pages/ServicePage";
+import PortfolioPage from "../pages/PortfolioPage";
+import ContactPage from "../pages/ContactPage";
+
+class AppRouter extends Component {
+  render() {
+    return (
+      <Router>
+        <Fragment>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/services" element={<ServicePage />} />
+            <Route path="/portfolio" element={<PortfolioPage />} />
+            <Route path="/about" element={<AboutPage />} />
+            <Route path="/contact" element={<ContactPage />} />
+          </Routes>
+        </Fragment>
+      </Router>
+    );
+  }
+}
+
+export default AppRouter;
